5230689d5249eb01ac21e0783d8b286d8bd838e7
Vadim <vgzulus@gmail.com>
2012-06-22 12:05:58 +0600

    1st

---
+++ MTrade/src/com/vkassin/mtrade/MTradeActivity.java
@@ -0,0 +1,13 @@
+package com.vkassin.mtrade;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MTradeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
