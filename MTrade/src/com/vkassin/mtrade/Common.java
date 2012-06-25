3fc192de4db8f4f72b442e62e5aeb4aa9c36fe4b
Vadim <vgzulus@gmail.com>
2012-06-25 18:55:30 +0600

    3

--- MTrade/src/com/vkassin/mtrade/Common.java
+++ MTrade/src/com/vkassin/mtrade/Common.java
@@ -1,11 +1,91 @@
 package com.vkassin.mtrade;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
 import android.widget.TabHost;
 
 public class Common {
 
 	private static final String TAG = "MTrade.Common"; 
+    private static final int HTTP_STATUS_OK = 200;
+	private static String sUserAgent = null;
+    private static byte[] sBuffer = new byte[4096];   
 
+    public final static Integer HEARTBEAT = 10;
+    public final static Integer LOGIN = 11;
+    public final static Integer LOGOUT = 12;
+
+    public final static Integer INSTRUMENT = 100;
+    public final static Integer TRADEACCOUNT = 101;
+    public final static Integer QUOTE = 102;
+    public final static Integer DEAL = 103;
+    public final static Integer TRANSIT_ORDER = 104;
+    public final static Integer CREATE_REMOVE_ORDER = 105;
+    public final static Integer CHART = 106;
+    
 	public static TabHost tabHost;
+
+	public static class ApiException extends Exception {
+	
+		public ApiException(String detailMessage, Throwable throwable) {
+	    
+			super(detailMessage, throwable);
+		}
+		public ApiException(String detailMessage) {
+	    
+			super(detailMessage);
+		}
+	}
+	
+    protected static synchronized String getUrlContent(String url) throws ApiException {
+    	
+        if (sUserAgent == null) {
+            throw new ApiException("User-Agent string must be prepared");
+        }
+
+        // Create client and set our specific user-agent string
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+        request.setHeader("User-Agent", sUserAgent);
+
+        try {
+            HttpResponse response = client.execute(request);
+
+            // Check if server response is valid
+            StatusLine status = response.getStatusLine();
+            if (status.getStatusCode() != HTTP_STATUS_OK) {
+                throw new ApiException("Invalid response from server: " +
+                        status.toString());
+            }
+
+            // Pull content stream from response
+            HttpEntity entity = response.getEntity();
+            InputStream inputStream = entity.getContent();
+
+            ByteArrayOutputStream content = new ByteArrayOutputStream();
+
+            // Read response into a buffered stream
+            int readBytes = 0;
+            while ((readBytes = inputStream.read(sBuffer)) != -1) {
+                content.write(sBuffer, 0, readBytes);
+            }
+
+            // Return result from buffered stream
+            return new String(content.toByteArray());
+        } catch (IOException e) {
+        	Log.e(TAG, "Error:",e);
+            throw new ApiException("Problem communicating with API", e);
+        }
+    }
 
 }
--- MTrade/src/com/vkassin/mtrade/InstrActivity.java
+++ MTrade/src/com/vkassin/mtrade/InstrActivity.java
@@ -1,7 +1,30 @@
 package com.vkassin.mtrade;
 
+import java.util.Calendar;
+
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 
 public class InstrActivity extends Activity {
 
+	private static final String TAG = "MTrade.InstrActivity"; 
+	
+    public void onCreate(Bundle savedInstanceState) {
+    	
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+        
+        JSONObject json = new JSONObject();
+        try{
+        json.put("time", Calendar.getInstance().getTimeInMillis());
+        json.put("objType", new Integer(Common.INSTRUMENT));
+        
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            Log.(TAG, "Error! Cannot Estabilish Connection", e);
+        }
 }
